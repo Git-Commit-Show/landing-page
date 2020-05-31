18e6ebeee220704c5c3ae1aca6caeedbb5f7406e
Parth Krishna <thenerdsuperuser@gmail.com>
2020-05-31 07:47:35 +0530

    Added carousel with slick

---
+++ assets/js/slick-effect.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		infinite: true,
+		autoplay: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		nextArrow: "<div class='slick-prev'><i class='fa fa-arrow-circle-right'></i></div>",
+		prevArrow: "<div class='slick-next'><i class='fa fa-arrow-circle-left'></i></div>"
+	});
+});
