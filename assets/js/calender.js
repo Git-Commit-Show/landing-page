b7425c60956cdc3837b743e916606972d292fd7f
Sarvesh Khandelwal <khandelwalsarvesh8@gmail.com>
2020-06-06 18:27:29 +0530

    Add to Calender solved

---
+++ assets/js/calender.js
@@ -0,0 +1,10 @@
+window.addeventasync = function(){
+    addeventstc.settings({
+        appleical  : {show:true, text:"Apple Calendar"},
+        google     : {show:true, text:"Google "},
+        office365  : {show:true, text:"Office 365"},
+        outlook    : {show:true,text:"Outlook"},
+        outlookcom : {show:true, text:"Outlook.com"},
+        yahoo      : {show:true, text:"Yahoo "}
+    });
+};
