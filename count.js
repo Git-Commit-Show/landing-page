2ad7c0820eb76708699a298d49b4c8112abbc3fe
Kritika Sahni <kritikasahni8@gmail.com>
2020-06-07 17:00:50 +0530

    Added countdown files

---
+++ count.js
@@ -0,0 +1,19 @@
+
+var second = 1000,
+minute = second * 60,
+hour = minute * 60,
+day = hour * 24;
+
+var countDown = new Date('Jun 27, 2020 10:00:00').getTime(),
+x = setInterval(function() {
+
+var now = new Date().getTime(),
+	distance = countDown - now;
+
+document.getElementById('days').innerText = Math.floor(distance / (day)),
+  document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+  document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+  document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+
+}, second)
