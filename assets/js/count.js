c82b7f4be126c3eeb760c100982e60e207950b18
kritikasahni8 <62658033+kritikasahni8@users.noreply.github.com>
2020-06-13 20:38:41 +0530

    corousal description and readmore updated,button removed

--- assets/js/count.js
+++ assets/js/count.js
@@ -16,6 +16,24 @@ document.getElementById('days').innerText = Math.floor(distance / (day)),
 
 }, second)
 
+document.addEventListener('DOMContentLoaded',()=>{
+  const expandsMore=document.querySelectorAll('[expand-more]')
+
+  function expand(){
+    const showContent=document.getElementById(this.dataset.target)
+    if(showContent.classList.contains('expand-active')){
+      this.innerHTML=this.dataset.showtext
+    }else{
+      this.innerHTML=this.dataset.hidetext
+    }
+    showContent.classList.toggle('expand-active')
+  }
+  expandsMore.forEach(expandMore => {
+    expandMore.addEventListener('click',expand)
+  })
+})
+
+/*
 // READ MORE FUNCTION
 function myFunction1() {
   var dots = document.getElementById("dots1");
@@ -77,3 +95,4 @@ function myFunction4() {
     moreText.style.display = "inline";
   }
 }
+*/
