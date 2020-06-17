254c52d8ecb7a60705536a8f6669d27bfa2c7c38
aryan2574 <ayush.aryan94@yahoo.com>
2020-06-17 16:09:08 +0530

    Changed Carousel and removed description

--- assets/js/count.js
+++ assets/js/count.js
@@ -16,22 +16,3 @@ document.getElementById('days').innerText = Math.floor(distance / (day)),
   document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
 }, second)
-
-//carousal js code
-
-document.addEventListener('DOMContentLoaded',()=>{
-  const expandsMore=document.querySelectorAll('[expand-more]')
-
-  function expand(){
-    const showContent=document.getElementById(this.dataset.target)
-    if(showContent.classList.contains('expand-active')){
-      this.innerHTML=this.dataset.showtext
-    }else{
-      this.innerHTML=this.dataset.hidetext
-    }
-    showContent.classList.toggle('expand-active')
-  }
-  expandsMore.forEach(expandMore => {
-    expandMore.addEventListener('click',expand)
-  })
-})
