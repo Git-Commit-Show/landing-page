871f751f32303a4e264aa7816379ad25090db1f3
Khaja Takreem Ulla <90376390+khajatakreemulla@users.noreply.github.com>
2021-12-03 23:10:11 +0530

    Update swithchtime.js

--- assets/js/swithchtime.js
+++ assets/js/swithchtime.js
@@ -5,12 +5,11 @@
 }
 
 
-document.querySelectorAll(".timezone-text").forEach(elm=>{
-    elm.innerText = "* Time shown is in "+ getTimeZone() + " Timezone"
-}) 
+document.querySelector(".timezone-text").innerText = "* Time shown is in "+ getTimeZone() + " Timezone"
+
 document.querySelectorAll(".duration").forEach((data , index)=>{
 date = data.getAttribute("data-date");
 let time = new Date(date)
 let x  = document.querySelectorAll(".speakers-time")
 x[index].innerHTML = time.toLocaleString("en-US", {timeZone: getTimeZone(), timeStyle: "short", hourCycle: "h24"})
-})
+});
