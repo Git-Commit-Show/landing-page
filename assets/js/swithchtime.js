8c2f0e5b94b51d0dad6e9439ae5890213344fdf0
Khaja Takreem Ulla <90376390+khajatakreemulla@users.noreply.github.com>
2021-12-03 14:55:25 +0530

    Update swithchtime.js

--- assets/js/swithchtime.js
+++ assets/js/swithchtime.js
@@ -5,7 +5,9 @@
 }
 
 
-document.querySelector(".timezone-text").innerText ="* Time shown is in "+ getTimeZone() + " Timezone"
+document.querySelectorAll(".timezone-text").forEach(elm=>{
+    elm.innerText = "* Time shown is in "+ getTimeZone() + " Timezone"
+}) 
 document.querySelectorAll(".duration").forEach((data , index)=>{
 date = data.getAttribute("data-date");
 let time = new Date(date)
